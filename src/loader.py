2442166521d50c6d7be3bf2b6efb036c8b2c9161
Bernardo Ferrari <ber.97ferrari@gmail.com>
2026-06-19 19:11:41 +0200

    fix: remove the prompts for options to filter and categorize from loader
    will move them to classifier

--- src/loader.py
+++ src/loader.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 def load_df(filepath):
     df = pd.read_csv(filepath)
+    filter_options = []
 
     print("Are you calculating spending or income?")
     
@@ -11,27 +12,15 @@ def load_df(filepath):
         filter_type = input("If spending write 's', if income write 'i': ")
         
         if filter_type.lower() == 's':  
-            print("I can filter out up to 4 transaction for you if you'd like")
-            print("Any string will work")
-            filter_argument_1 = input("Filter option 1: ")
-            filter_argument_2 = input("Filter option 2: ")
-            filter_argument_3 = input("Filter option 3: ")
-            filter_argument_4 = input("Filter option 4: ")
-            
             # Get only transactions with negative numbers (< 0)
             df_spending = df[
-                (df["Transactiebedrag"] < 0) &
-                (~df["Omschrijving"].str.contains(filter_argument_1, case=False)) &
-                (~df["Omschrijving"].str.contains(filter_argument_2, case=False)) &
-                (~df["Omschrijving"].str.contains(filter_argument_3, case=False)) &
-                (~df["Omschrijving"].str.contains(filter_argument_4, case=False))
-            ]
+                (df["Transactiebedrag"] < 0)]
 
-            return df_spending, "s"
+            return df_spending, "s", filter_options
         elif filter_type.lower() == 'i':
             # Get only transactions with positive numbers (> 0)
             df_income = df[(df["Transactiebedrag"] > 0)]
 
-            return df_income, "i"
+            return df_income, "i", []
         else:
          print("Looks like you selected a different operation I can't help you with :(")
